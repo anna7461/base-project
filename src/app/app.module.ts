32f7f55084b6d70b4e0f69793a3fd1c0aac6798e
Anna Chilingaryan <a.ch7461@gmail.com>
2021-10-05 00:59:11 +0400

    NNgroup initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NngroupComponent } from './projects/nngroup/nngroup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NngroupComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/projects/nngroup/nngroup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nngroup',
+  templateUrl: './nngroup.component.html',
+  styleUrls: ['./nngroup.component.scss']
+})
+export class NngroupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
