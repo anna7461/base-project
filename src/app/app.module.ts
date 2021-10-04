b7c25c3154330875367c5bd4dc7b8eb9660de4a2
Anna Chilingaryan <a.ch7461@gmail.com>
2021-10-05 00:59:55 +0400

    Freecodecamp initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FreecodecampComponent } from './projects/freecodecamp/freecodecamp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FreecodecampComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/projects/freecodecamp/freecodecamp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-freecodecamp',
+  templateUrl: './freecodecamp.component.html',
+  styleUrls: ['./freecodecamp.component.scss']
+})
+export class FreecodecampComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
