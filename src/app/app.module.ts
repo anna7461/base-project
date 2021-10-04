d9bfcada3e812ed032b592c850b5886dd4a84fde
Anna Chilingaryan <a.ch7461@gmail.com>
2021-10-05 01:00:35 +0400

    Toptal initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToptalComponent } from './projects/toptal/toptal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToptalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/projects/toptal/toptal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toptal',
+  templateUrl: './toptal.component.html',
+  styleUrls: ['./toptal.component.scss']
+})
+export class ToptalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
