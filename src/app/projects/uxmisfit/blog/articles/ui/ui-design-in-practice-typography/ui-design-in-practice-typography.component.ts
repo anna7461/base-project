f07fa86f95f6ee263f83f7d5717cd66a16cd6c70
Anna Chilingaryan <a.ch7461@gmail.com>
2022-01-14 02:37:25 +0400

    Uxmisfit.com UI design in practice typography article

---
+++ src/app/projects/uxmisfit/blog/articles/ui/ui-design-in-practice-typography/ui-design-in-practice-typography.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-design-in-practice-typography',
+  templateUrl: './ui-design-in-practice-typography.component.html',
+  styleUrls: ['./ui-design-in-practice-typography.component.scss']
+})
+export class UiDesignInPracticeTypographyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/projects/uxmisfit/uxmisfit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-uxmisfit',
+  templateUrl: './uxmisfit.component.html',
+  styleUrls: ['./uxmisfit.component.scss']
+})
+export class UxmisfitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
