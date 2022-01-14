f82523be58801389126ca16e395002324de7ef2f
Anna Chilingaryan <a.ch7461@gmail.com>
2022-01-14 15:22:26 +0400

    Is it worth learning with ui tips article

---
+++ src/app/projects/uxmisfit/blog/articles/ui/tips/is-it-worth-learning-with-ui-tips/is-it-worth-learning-with-ui-tips.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-is-it-worth-learning-with-ui-tips',
+  templateUrl: './is-it-worth-learning-with-ui-tips.component.html',
+  styleUrls: ['./is-it-worth-learning-with-ui-tips.component.scss']
+})
+export class IsItWorthLearningWithUiTipsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
