80331e8071a1e01698b12cb043610250d1704433
Anna Chilingaryan <a.ch7461@gmail.com>
2022-01-18 06:23:12 +0400

    Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {
+  BuildingDesignSystemPartThreeColorsComponent
+} from './projects/uxmisfit/blog/articles/design-systems/building-design-system-part-three-colors/building-design-system-part-three-colors.component';
+import {
+  IsItWorthLearningWithUiTipsComponent
+} from './projects/uxmisfit/blog/articles/ui/tips/is-it-worth-learning-with-ui-tips/is-it-worth-learning-with-ui-tips.component';
+import {
+  UiDesignInPracticeTypographyComponent
+} from './projects/uxmisfit/blog/articles/ui/ui-design-in-practice-typography/ui-design-in-practice-typography.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'building_design_system_part_three_colors',
+    pathMatch: 'full',
+    component: BuildingDesignSystemPartThreeColorsComponent,
+  },
+
+  {
+    path: 'is_it_worth_learning_with_ui_tips',
+    pathMatch: 'full',
+    component: IsItWorthLearningWithUiTipsComponent,
+  },
+
+  {
+    path: 'ui_design_practice_typography',
+    pathMatch: 'full',
+    component: UiDesignInPracticeTypographyComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/projects/uxmisfit/blog/articles/design-systems/building-design-system-part-three-colors/building-design-system-part-three-colors.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-building-design-system-part-three-colors',
+  templateUrl: './building-design-system-part-three-colors.component.html',
+  styleUrls: ['./building-design-system-part-three-colors.component.scss']
+})
+export class BuildingDesignSystemPartThreeColorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
