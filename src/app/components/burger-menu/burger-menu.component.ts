4f2c7d59a89695e8ac11e176da9ffbb945b0c83c
Anna Chilingaryan <a.ch7461@gmail.com>
2022-02-24 02:25:04 +0400

    burger menu created + animation

---
+++ src/app/components/burger-menu/burger-menu.component.ts
@@ -0,0 +1,12 @@
+// export class AppComponent {
+//
+//   public toggled: boolean = false;
+//
+//   toggleBurger() {
+//     if (!this.toggled) {
+//       return this.toggled = true;
+//     } else {
+//       return this.toggled = false;
+//     }
+//   }
+// }
